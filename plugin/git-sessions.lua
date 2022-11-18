85ea09760666b69be2c14b79a87d45318903ecd4
Timothee Saisset <timothee.saisset@spacefill.fr>
2022-11-18 16:56:29 +0100

    feat: first commit

---
+++ plugin/git-sessions.lua
@@ -0,0 +1,35 @@
+-- make sure this file is loaded only once
+if vim.g.loaded_my_awesome_plugin == 1 then
+  return
+end
+vim.g.loaded_my_awesome_plugin = 1
+
+-- create any global command that does not depend on user setup
+-- usually it is better to define most commands/mappings in the setup function
+-- Be careful to not overuse this file!
+local git_sessions = require'git-sessions'
+
+vim.api.nvim_create_user_command(
+    "SaveSession",
+    git_sessions.save_session,
+    {}
+)
+
+vim.api.nvim_create_user_command(
+    "LoadSession",
+    git_sessions.load_session,
+    {}
+)
+
+vim.api.nvim_create_user_command(
+    "SelectSession",
+    git_sessions.load_session,
+    {}
+)
+
+vim.api.nvim_create_user_command(
+    "DeleteSession",
+    git_sessions.ui_delete_session,
+    {}
+)
+
